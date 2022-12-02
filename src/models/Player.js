5393ddc1206e2d27e7b666234503b1f89342567a
chex <chex417@naver.com>
2022-12-03 01:31:52 +0900

    refactor: 플레이어의 입력을 숫자배열로 변경하는 기능 분리

--- src/models/Player.js
+++ src/models/Player.js
@@ -5,8 +5,12 @@ class Player {
   #numbers;
 
   renew(input) {
-    this.#numbers = input;
+    this.#numbers = this.makeNumbers(input);
     if (!this.validate(input)) throw new Error(ERROR_MESSAGE.invalidInput);
+  }
+
+  makeNumbers(input) {
+    return [...input].map((str) => parseInt(str));
   }
 
   validate(input) {
@@ -23,11 +27,11 @@ class Player {
   getScore(computer) {
     const score = { strike: 0, ball: 0 };
     for (let i = 0; i < this.#numbers.length; i++) {
-      if (computer[i] === parseInt(this.#numbers[i])) {
+      if (computer[i] === this.#numbers[i]) {
         score.strike += 1;
         continue;
       }
-      if (computer.includes(parseInt(this.#numbers[i]))) score.ball += 1;
+      if (computer.includes(this.#numbers[i])) score.ball += 1;
     }
     return Object.freeze(score);
   }
